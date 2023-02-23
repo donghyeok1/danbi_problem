1fb92821ce9468e597aca2848118eb71ae66034f
donghyeok1 <95459089+donghyeok1@users.noreply.github.com>
2023-02-23 16:08:36 +0900

    mysql settings

--- danbi_problem/settings.py
+++ danbi_problem/settings.py
@@ -86,11 +86,17 @@ WSGI_APPLICATION = 'danbi_problem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'danbi',
+        'USER': 'root',
+        'PASSWORD': 'qwer1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
-
 AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
